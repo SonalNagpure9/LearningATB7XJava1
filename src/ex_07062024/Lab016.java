1c8d1ed17fef04b988e63516630a5cb2f6626397
Sonal Nagpure <sonalgnagpure@gmail.com>
2024-07-24 21:28:24 +0530

    all files added

---
+++ src/ex_07062024/Lab016.java
@@ -0,0 +1,13 @@
+package ex_07062024;
+
+public class Lab016 {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+        int b = 20;
+        System.out.println(b);
+        //Print sum of a and b
+        System.out.println(a+b);
+
+    }
+}

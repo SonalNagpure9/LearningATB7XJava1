a764aa1675ec0d664c6c3c815007d498a16b6d73
Sonal Nagpure <sonalgnagpure@gmail.com>
2024-08-27 09:53:48 +0530

    all files added

---
+++ src/ex_13072024/Lab019.java
@@ -0,0 +1,15 @@
+package ex_13072024;
+
+public class Lab019 {
+    public static void main(String[] args) {
+        //age
+        byte age = 122; //less memory
+        int age1 = 100; //waste memory
+        //program to store float
+        float pi = 3.14f;
+        double pi_d = 3.14567893333;
+
+
+
+    }
+}

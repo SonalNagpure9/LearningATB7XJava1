127e5975ab460e34bb068930f1055908fabee601
Sonal Nagpure <sonalgnagpure@gmail.com>
2024-09-02 15:45:18 +0530

    all files added

---
+++ src/ex_13072024/Lab026.java
@@ -0,0 +1,28 @@
+package ex_13072024;
+
+public class Lab026 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+   //     System.out.println(a+b);
+
+    //    String s1 = "sonal";
+     //   String s2 = "I";
+     //   System.out.println(s1+s2);
+
+        String s3 = "Amit";
+        int x = 99;
+        int y = 100;
+        System.out.println(x+y);
+        System.out.println(x+s3);
+        System.out.println(s3+y);
+        System.out.println(x+y+s3);
+        System.out.println(s3+x+y);
+        System.out.println(x+s3+y);
+        System.out.println(x+y+s3+y+s3+x+y);
+
+
+
+
+    }
+}
---
+++ src/ex_13072024/Lab028.java
@@ -0,0 +1,12 @@
+package ex_13072024;
+
+public class Lab028 {
+    public static void main(String[] args) {
+        int age = 10; //decimal  number system
+        int b = 0b1010; //binary
+        int octal = 0101;
+        int hex = 0Xface;
+
+
+    }
+}

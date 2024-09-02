c63d79e118a23f1c5829d865e50914380da87b2c
Sonal Nagpure <sonalgnagpure@gmail.com>
2024-09-02 20:11:48 +0530

    all files added

---
+++ src/ex_13072024/Lab029.java
@@ -0,0 +1,9 @@
+package ex_13072024;
+
+public class Lab029 {
+    public static void main(String[] args) {
+        int age  = 65;
+        String name2 = "Pramod";
+
+    }
+}
---
+++ src/ex_13072024/Lab030.java
@@ -0,0 +1,18 @@
+package ex_13072024;
+
+public class Lab030 {
+    public static void main(String[] args) {
+        double a = 10;
+        double b = 34;
+        double sum = a+b;
+        double sub = a-b;
+        double mul = a*b;
+        double div = a/b;
+        System.out.printf("sum is %f\n", sum);
+        System.out.printf("sub is %f\n", sub);
+        System.out.printf("mul is %f\n", mul);
+        System.out.printf("div is %f\n", div);
+
+
+    }
+}

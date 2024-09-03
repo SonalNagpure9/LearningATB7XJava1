edad051f813823f3b58e827690c009600a7ee391
Sonal Nagpure <sonalgnagpure@gmail.com>
2024-09-03 13:44:36 +0530

    all files added

---
+++ src/ex_13072024/Lab034.java
@@ -0,0 +1,19 @@
+package ex_13072024;
+
+public class Lab034 {
+    public static void main(String[] args) {
+        double a = 34;
+        double b = 10;
+        double result = a%b; //modulus operator  - Remainder
+        System.out.println(result);
+   // 10  | 34| 3 Q
+      //  | 30 |
+        //   4  Remainder
+
+
+        System.out.println(89/10);
+        System.out.println(89%10);
+
+
+    }
+}
---
+++ src/ex_13072024/Lab036.java
@@ -0,0 +1,9 @@
+package ex_13072024;
+
+public class Lab036 {
+    public static void main(String[] args) {
+        char c= '\n';
+        System.out.println(c);
+        byte Byte = 10;
+    }
+}

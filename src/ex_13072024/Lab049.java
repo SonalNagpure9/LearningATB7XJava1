b650237813539b07fc0c839f5370ddc114e708a3
Sonal Nagpure <sonalgnagpure@gmail.com>
2024-09-16 17:38:06 +0530

    all files added

---
+++ src/ex_13072024/Lab049.java
@@ -0,0 +1,12 @@
+package ex_13072024;
+
+public class Lab049 {
+    public static void main(String[] args) {
+        // And && //True and true returns true
+        //  T ||T
+        System.out.println(true &&  true);
+        System.out.println(true && false);
+        System.out.println(false && false);
+        System.out.println(false && false);
+    }
+}
